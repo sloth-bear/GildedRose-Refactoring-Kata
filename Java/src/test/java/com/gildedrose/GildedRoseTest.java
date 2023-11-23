e2c63680e22fba1d1f893b8fd3a41f73a3ed3e53
sloth-bear <slothbear.hj@gmail.com>
2023-11-23 17:20:07 +0900

    Add normal test cases

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -8,6 +8,7 @@ class GildedRoseTest {
 
     private static final String AGED_BRIE_CHEESE_NAME = "Aged Brie";
     private static final String BACKSTAGE_PASSES_NAME = "Backstage passes to a TAFKAL80ETC concert";
+    private static final String SOME_ITEM_NAME = "item";
 
     @Test
     void givenItemFoo_whenUpdateQuality_thenNotChangeItems() {
@@ -15,6 +16,83 @@ class GildedRoseTest {
         GildedRose app = new GildedRose(items);
         app.updateQuality();
         assertEquals("foo", app.items[0].name);
+    }
+
+    @Test
+    void givenSomeItemSellInExceed0ExceptAgedBrieAndBackstagePasses_whenUpdateQuality_thenQuality1Decreased() {
+        // given
+        final Item[] items1 = new Item[] { new Item(SOME_ITEM_NAME, 1, 3) };
+        GildedRose app1 = new GildedRose(items1);
+
+        final Item[] items2 = new Item[] { new Item(SOME_ITEM_NAME, 3, 1) };
+        GildedRose app2 = new GildedRose(items2);
+
+        // when
+        app1.updateQuality();
+        app2.updateQuality();
+
+        // then
+        assertEquals(2, app1.items[0].quality);
+        assertEquals(0, app2.items[0].quality);
+    }
+
+    @Test
+    void givenSomeItemSellInLessThan0ExceptAgedBrieAndBackstagePasses_whenUpdateQuality_thenQuality2Decreased() {
+        // given
+        final Item[] items1 = new Item[] { new Item(SOME_ITEM_NAME, 0, 3) };
+        GildedRose app1 = new GildedRose(items1);
+
+        final Item[] items2 = new Item[] { new Item(SOME_ITEM_NAME, -2, 2) };
+        GildedRose app2 = new GildedRose(items2);
+
+        // when
+        app1.updateQuality();
+        app2.updateQuality();
+
+        // then
+        assertEquals(1, app1.items[0].quality);
+        assertEquals(0, app2.items[0].quality);
+    }
+
+    @Test
+    void givenSomeRandomItem_whenUpdateQuality_thenSellIn1Decreased() {
+        // given
+        final Item[] items1 = new Item[] { new Item(SOME_ITEM_NAME, 1, 3) };
+        GildedRose app1 = new GildedRose(items1);
+
+        final Item[] items2 = new Item[] { new Item(BACKSTAGE_PASSES_NAME, -1, 1) };
+        GildedRose app2 = new GildedRose(items2);
+
+        final Item[] items3 = new Item[] { new Item(AGED_BRIE_CHEESE_NAME, 0, 1) };
+        GildedRose app3 = new GildedRose(items3);
+
+        // when
+        app1.updateQuality();
+        app2.updateQuality();
+        app3.updateQuality();
+
+        // then
+        assertEquals(0, app1.items[0].sellIn);
+        assertEquals(-2, app2.items[0].sellIn);
+        assertEquals(-1, app3.items[0].sellIn);
+    }
+
+    @Test
+    void givenSomeRandomItemQualityLessThan0ExceptAgedBrieAndBackstagePasses_whenUpdateQuality_thenQualityNotDecreased() {
+        // given
+        final Item[] items1 = new Item[] { new Item(SOME_ITEM_NAME, 1, 0) };
+        GildedRose app1 = new GildedRose(items1);
+
+        final Item[] items2 = new Item[] { new Item(SOME_ITEM_NAME, -1, -1) };
+        GildedRose app2 = new GildedRose(items2);
+
+        // when
+        app1.updateQuality();
+        app2.updateQuality();
+
+        // then
+        assertEquals(0, app1.items[0].quality);
+        assertEquals(-1, app2.items[0].quality);
     }
 
     @Test
