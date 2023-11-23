a141c767677c9608d7b5036300b079e04b2dbf18
sloth-bear <slothbear.hj@gmail.com>
2023-11-23 16:53:08 +0900

    Add Backstage passes Quality tests

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -7,6 +7,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class GildedRoseTest {
 
     private static final String AGED_BRIE_CHEESE_NAME = "Aged Brie";
+    private static final String BACKSTAGE_PASSES_NAME = "Backstage passes to a TAFKAL80ETC concert";
 
     @Test
     void givenItemFoo_whenUpdateQuality_thenNotChangeItems() {
@@ -55,6 +56,78 @@ class GildedRoseTest {
         assertEquals(0, app1.items[0].sellIn);
         assertEquals(2, app2.items[0].sellIn);
         assertEquals(-1, app3.items[0].sellIn);
+    }
+
+    @Test
+    void givenItemBackstagePassesUnderQuality50AndExceedSellIn10_whenUpdateQuality_thenQuality1Increased() {
+        // given
+        final Item[] items1 = new Item[] { new Item(BACKSTAGE_PASSES_NAME, 11, 2) };
+        GildedRose app1 = new GildedRose(items1);
+
+        final Item[] items2 = new Item[] { new Item(BACKSTAGE_PASSES_NAME, 78, 0) };
+        GildedRose app2 = new GildedRose(items2);
+
+        // when
+        app1.updateQuality();
+        app2.updateQuality();
+
+        // then
+        assertEquals(3, app1.items[0].quality);
+        assertEquals(1, app2.items[0].quality);
+    }
+
+    @Test
+    void givenItemBackstagePassesUnderQuality50AndBetweenSellIn6And10_whenUpdateQuality_thenQuality2Increased() {
+        // given
+        final Item[] items1 = new Item[] { new Item(BACKSTAGE_PASSES_NAME, 10, 2) };
+        GildedRose app1 = new GildedRose(items1);
+
+        final Item[] items2 = new Item[] { new Item(BACKSTAGE_PASSES_NAME, 6, 0) };
+        GildedRose app2 = new GildedRose(items2);
+
+        // when
+        app1.updateQuality();
+        app2.updateQuality();
+
+        // then
+        assertEquals(4, app1.items[0].quality);
+        assertEquals(2, app2.items[0].quality);
+    }
+
+    @Test
+    void givenItemBackstagePassesUnder50QualityAndUnderSellIn5_whenUpdateQuality_thenQuality3Increased() {
+        // given
+        final Item[] items1 = new Item[] { new Item(BACKSTAGE_PASSES_NAME, 5, 2) };
+        GildedRose app1 = new GildedRose(items1);
+
+        final Item[] items2 = new Item[] { new Item(BACKSTAGE_PASSES_NAME, 1, 0) };
+        GildedRose app2 = new GildedRose(items2);
+
+        // when
+        app1.updateQuality();
+        app2.updateQuality();
+
+        // then
+        assertEquals(5, app1.items[0].quality);
+        assertEquals(3, app2.items[0].quality);
+    }
+
+    @Test
+    void givenItemBackstagePassesUnder50QualityAndUnderSellIn0_whenUpdateQuality_thenQuality0() {
+        // given
+        final Item[] items1 = new Item[] { new Item(BACKSTAGE_PASSES_NAME, 0, 4) };
+        GildedRose app1 = new GildedRose(items1);
+
+        final Item[] items2 = new Item[] { new Item(BACKSTAGE_PASSES_NAME, -1, 6) };
+        GildedRose app2 = new GildedRose(items2);
+
+        // when
+        app1.updateQuality();
+        app2.updateQuality();
+
+        // then
+        assertEquals(0, app1.items[0].quality);
+        assertEquals(0, app2.items[0].quality);
     }
 
 }
